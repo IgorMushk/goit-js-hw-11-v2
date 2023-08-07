5dd9c66372487c39f77ea9e71bfc18d1ae92cb99
IgorMushk <igor.mushketik@gmail.com>
2023-08-07 22:56:43 +0300

    Copy  from ver.2
    From C:\WORK\GoIT\JS\mod10-11-hw-11\src\03-index.js + scss

--- src/index.js
+++ src/index.js
@@ -20,12 +20,38 @@ const simpleLightBoxLightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 
-// let options = {
-//   root: null,
-//   rootMargin: '300px',
-//   threshold: 1.0,
-// };
-// let observer = new IntersectionObserver(onLoadInfinitiScroll, options);
+let options = {
+  root: null,
+  // 300px ->100px
+  rootMargin: '100px',
+  threshold: 1.0,
+};
+let observer = new IntersectionObserver(onLoadInfinitiScroll, options);
+
+const btnUp = {
+  element: document.querySelector('.btn-up'),
+  show() {
+    this.element.hidden = false;
+  },
+  hide() {
+    this.element.hidden = true;
+  },
+  addEventListener() {
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      scrollY > 500 ? this.show() : this.hide();
+    });
+    document.querySelector('.btn-up').onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    };
+  },
+};
+
+btnUp.addEventListener();
 
 formSearchRef.addEventListener('submit', onSubmitSearch);
 
@@ -39,84 +65,71 @@ function onSubmitSearch(event) {
     return Notify.failure('Enter a search string');
   }
 
-  fetchImages(searchQuery, currentPage, PER_PAGE)
-    .then(data => {
-      if (!data.hits.length & !data.totalHits) {
-        loadMoreRef.classList.add('is-hidden');
-        return Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      totalHits = data.totalHits;
-      if (currentPage === 1) {
-        Notify.success(`Hooray! We found ${totalHits} images.`);
-      }
-
-      renderGallery(data.hits);
-      simpleLightBoxLightbox.refresh();
-      //observer.observe(target); // + for infiniti scroll
-
-      if (data.hits.length === data.totalHits) {
-        // 'zaz'
-        loadMoreRef.classList.add('is-hidden');
-        Notify.warning(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-      if (data.hits.length < data.totalHits) {
-        loadMoreRef.classList.remove('is-hidden'); //- for Btn  loadMore
-      }
-    })
-    .catch(err => console.log(err));
+  getData(searchQuery, currentPage, PER_PAGE);
 }
 
-loadMoreRef.addEventListener('click', onLoad); //
-
-async function onLoad() {
-  currentPage += 1;
+// function getData() -> dataServ.js
+async function getData(searchQuery, currentPage, PER_PAGE) {
+  //fetchImages(searchQuery, currentPage, PER_PAGE)
   try {
     const data = await fetchImages(searchQuery, currentPage, PER_PAGE);
+    //-.then(data => {
+    console.log(currentPage);
+    if (!data.hits.length & !data.totalHits) {
+      loadMoreRef.classList.add('is-hidden');
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    totalHits = data.totalHits;
+    if (currentPage === 1) {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+
     renderGallery(data.hits);
     simpleLightBoxLightbox.refresh();
+    observer.observe(target); // + for infiniti scroll
 
+    //   if (data.hits.length === data.totalHits) {
+    //     // 'zaz'
+    //     loadMoreRef.classList.add('is-hidden');
+    //     Notify.warning(
+    //       "We're sorry, but you've reached the end of search results."
+    //     );
+    //   }
+    if (data.hits.length < data.totalHits) {
+      //loadMoreRef.classList.remove('is-hidden'); //- for Btn  loadMore
+    }
+    // -2 load
     if (currentPage === Math.ceil(data.totalHits / PER_PAGE)) {
       loadMoreRef.classList.add('is-hidden');
       Notify.warning(
         "We're sorry, but you've reached the end of search results."
       );
+      observer.unobserve(target); // +
     } else {
-      loadMoreRef.classList.remove('is-hidden'); // -
+      //loadMoreRef.classList.remove('is-hidden'); // -
     }
-    smoothScroll(galleryRef);
+    //   if (currentPage > 1) {
+    //     console.log(currentPage);
+    //     smoothScroll(galleryRef);
+    //   }
+    //-})
+    //-.catch(err => console.log(err));
   } catch (err) {
     console.log(err);
   }
 }
 
-// function onLoadInfinitiScroll(entries, observer) {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//       currentPage += 1;
-//       fetchImages(searchQuery, currentPage, PER_PAGE)
-//         .then(data => {
-//           renderGallery(data.hits);
-//           simpleLightBoxLightbox.refresh();
-//
-//           if (currentPage === Math.ceil(data.totalHits / PER_PAGE)) {
-//             loadMoreRef.classList.add('is-hidden');
-//             Notify.warning(
-//               "We're sorry, but you've reached the end of search results."
-//             );
-//             observer.unobserve(target); // +
-//           } else {
-//             //loadMoreRef.classList.remove('is-hidden'); // -
-//           }
-//           smoothScroll(galleryRef);
-//         })
-//         .catch(err => console.log(err));
-//     }
-//   });
-// }
+function onLoadInfinitiScroll(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+
+      getData(searchQuery, currentPage, PER_PAGE);
+    }
+  });
+}
 
 function renderGallery(dataArr) {
   galleryRef.insertAdjacentHTML('beforeend', createMarkup(dataArr));
